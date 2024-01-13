15c481c5e5f1d4b176848adbd7d76611fa2ac3f4
mhserrano <mariahcserrano@gmail.com>
2024-01-13 22:05:27 +0000

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 function handleSubmit(event) {
   event.preventDefault();
-
+  poem.classList.remove("hidden");
+  poem.innerHTML = `<i class="fa-solid fa-spinner fa-spin-pulse fa-2xl" style="color: #9bbec8;"></i>`;
   let userInstructions = document.querySelector("#user-instructions");
   let apiKey = "26fa1d65306c6427fb2f815to8d8b867";
   let prompt = `Generate a poem about ${userInstructions.value}`;
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a four-line stanza in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(showResults);
@@ -18,8 +19,8 @@ function showResults(response) {
     cursor: "",
     delay: 1,
   });
-  console.log(response);
 }
 
+let poem = document.querySelector("#poem");
 let poemFormElement = document.querySelector("#form");
 poemFormElement.addEventListener("submit", handleSubmit);
